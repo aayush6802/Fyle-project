d004420bf4a03bcc78fb5e13a1f5948c9c7d9123
aayush6802 <aayush6802@gmail.com>
2024-01-20 18:41:42 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        font1: ['Martian Mono'],
+        font2: ['Poppins']
+      }
+    },
+  },
+  plugins: [],
+}
+
